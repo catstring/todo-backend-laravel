107819e29678b1a9b98fadf04fc03c68b234050d
Todd MC Tsai <toddmctsai@gmail.com>
2024-06-03 22:26:24 +0800

    newkey

--- app/Http/Controllers/Api/V1/TaskController.php
+++ app/Http/Controllers/Api/V1/TaskController.php
@@ -1,58 +1,58 @@
 <?php
 
-namespace App\Http\Controllers\Api\V1;
+// namespace App\Http\Controllers\Api\V1;
 
-use App\Http\Controllers\Controller;
-use App\Http\Requests\StoreTaskRequest;
-use App\Http\Requests\UpdateTaskRequest;
-use App\Http\Resources\TaskResource;
-use App\Models\Task;
+// use App\Http\Controllers\Controller;
+// use App\Http\Requests\StoreTaskRequest;
+// use App\Http\Requests\UpdateTaskRequest;
+// use App\Http\Resources\TaskResource;
+// use App\Models\Task;
 
-class TaskController extends Controller
-{
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        return TaskResource::collection(Task::all());
-    }
+// class TaskController extends Controller
+// {
+//     /**
+//      * Display a listing of the resource.
+//      */
+//     public function index()
+//     {
+//         return TaskResource::collection(Task::all());
+//     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(StoreTaskRequest $request)
-    {
-        $task = Task::create($request->validated());
+//     /**
+//      * Store a newly created resource in storage.
+//      */
+//     public function store(StoreTaskRequest $request)
+//     {
+//         $task = Task::create($request->validated());
 
-        return TaskResource::make($task);
-    }
+//         return TaskResource::make($task);
+//     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(Task $task)
-    {
-        return TaskResource::make($task);
-    }
+//     /**
+//      * Display the specified resource.
+//      */
+//     public function show(Task $task)
+//     {
+//         return TaskResource::make($task);
+//     }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(UpdateTaskRequest $request, Task $task)
-    {
-        $task->update($request->validated());
+//     /**
+//      * Update the specified resource in storage.
+//      */
+//     public function update(UpdateTaskRequest $request, Task $task)
+//     {
+//         $task->update($request->validated());
 
-        return TaskResource::make($task);
-    }
+//         return TaskResource::make($task);
+//     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Task $task)
-    {
-        $task->delete();
+//     /**
+//      * Remove the specified resource from storage.
+//      */
+//     public function destroy(Task $task)
+//     {
+//         $task->delete();
 
-        return response()->noContent();
-    }
-}
+//         return response()->noContent();
+//     }
+// }

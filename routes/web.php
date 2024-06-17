ef4f99cbf9278178f49fe856423bcfc6fbe63fec
Todd MC Tsai <toddmctsai@gmail.com>
2024-06-17 21:48:32 +0800

    correct auth prefix

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::prefix('/auth')->group(function(){
+Route::prefix('auth')->group(function(){
     Route::post('/login', LoginController::class)->middleware('guest');
     Route::post('/logout', LogoutController::class);
     Route::post('/register', RegisterController::class)->middleware('guest');
